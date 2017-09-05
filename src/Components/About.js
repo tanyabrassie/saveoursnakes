977e05557927755486d59d0ddb007420f1a12963
Tanya Brassie <tanyabrassie@TANYASCMACHINE2.home>
2017-09-05 00:12:35 -0400

    Adding in more snake data, filtering snake data for home page and about page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Headline } from './ui/typography';
+import SnakeTiles from './SnakeTiles';
+import snakeData from '../snakedata';
 
+function filterSnakes (snake) {
+	return !snake.featured;
+}
+var filteredSnakes = snakeData.filter(filterSnakes);
+
+console.log (filteredSnakes);
 
 class About extends React.Component {
 	constructor() {
-	super();
-	this.state = {
-	  count: 0,
-	};
+		super();
+		this.state = {
+		  count: 0,
+		};
 	}
 
 	render() {
 		return (
-		<div>
-			<h1>This is the about page.</h1>
-		</div>
+		<section>
+			<Headline>This is the about page.</Headline>
+			<SnakeTiles snakeData={filteredSnakes}/>
+		</section>
 		);
 	};
 }	
